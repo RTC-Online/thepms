7f9194e5555cfe5caf88b994514f96dd5e30a69a
project-oneeye <janiba.pascal01@gmail.com>
2024-03-20 22:36:30 +0100

    UPDATED projects_wikis migration

--- database/migrations/2024_03_19_211025_create_project_wikis_table.php
+++ database/migrations/2024_03_19_211025_create_project_wikis_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('project_wikis', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
