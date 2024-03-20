b78e37a105bcb5d0e96c855cb46ecc90f3070ec4
project-oneeye <janiba.pascal01@gmail.com>
2024-03-20 22:40:11 +0100

    UPDATED time_bookings migration

--- database/migrations/2024_03_19_193658_create_time_bookings_table.php
+++ database/migrations/2024_03_19_193658_create_time_bookings_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('time_bookings', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('ticket_id');
+            $table->integer('category_id');
+            $table->float('hours');
+            $table->string('comment');
             $table->timestamps();
         });
     }
